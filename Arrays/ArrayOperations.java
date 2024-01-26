903f27e2753176009f7b57ec472697b17dc5cd66
Thejas <teju270934@gmail.com>
2024-01-26 15:42:48 +0530

    new DSA in JAVA folder

---
+++ Arrays/ArrayOperations.java
@@ -0,0 +1,20 @@
+public class ArrayOperations {
+
+  public void insertAtStart(){
+
+  }
+
+  public static void main(String[] args) {
+      
+  }
+}
+
+
+
+echo "# DSA-java-" >> README.md
+git init
+
+
+
+
+git push -u origin main
