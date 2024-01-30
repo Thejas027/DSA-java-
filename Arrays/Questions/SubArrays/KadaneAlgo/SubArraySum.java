53695a613de46e123fc8f94a6d0e8b7ec572d764
Thejas <teju270934@gmail.com>
2024-01-30 23:17:14 +0530

    comments added

--- Arrays/Questions/SubArrays/KadaneAlgo/SubArraySum.java
+++ Arrays/Questions/SubArrays/KadaneAlgo/SubArraySum.java
@@ -3,8 +3,8 @@ package KadaneAlgo;
 public class SubArraySum {
 
   public static void subArSum(int num[]) {
-    int ms = Integer.MIN_VALUE;
-    int cs = 0;
+    int ms = Integer.MIN_VALUE;  // maximum sum
+    int cs = 0;                  // current sum
     for (int i = 0; i < num.length; i++) {
       cs = cs + num[i];
       if (cs < 0) {
