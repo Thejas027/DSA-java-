4543a38b856d315b3bff0d87c7bea180dbf1fccc
Thejas <teju270934@gmail.com>
2024-02-08 23:37:20 +0530

    delete method added

--- LinkedLists/Linkedlist.java
+++ LinkedLists/Linkedlist.java
@@ -1,5 +1,5 @@
 
-package LinkedLists;
+// package LinkedLists;
 
 public class Linkedlist {
 
@@ -15,30 +15,31 @@ public class Linkedlist {
 
   public static Node head;
   public static Node tail;
-  int count = 0;
+  public int count = 0;
 
   public void addFirst(int data) {
     Node newnNode = new Node(data);
+    count++;
     if (head == null) {
       head = tail = newnNode;
-      count++;
+
       return;
     }
     newnNode.next = head;
     head = newnNode;
-    count++;
+
   }
 
   public void addLast(int data) {
     Node newNode = new Node(data);
+    count++;
     if (head == null) {
       head = tail = newNode;
-      count++;
+
       return;
     }
     tail.next = newNode;
     tail = newNode;
-    count++;
   }
 
   public void display() {
@@ -49,9 +50,10 @@ public class Linkedlist {
       return;
     }
     while (current != null) {
-      System.out.print(current.data + " ");
+      System.out.print(current.data + "->");
       current = current.next;
     }
+    System.out.println("null");
   }
 
   public void addAtPos(int data, int pos) {
@@ -59,20 +61,64 @@ public class Linkedlist {
       System.out.println("Invalid index\n");
       return;
     }
-    Node newNode = new Node(data);
-    Node current = head;
+
     int i = 0;
     if (pos == 0) {
       addFirst(data);
     } else if (pos == count) {
       addLast(data);
     } else {
+      Node newNode = new Node(data);
+      count++;
+      Node current = head;
       while (i < pos - 1) {
         current = current.next;
         i++;
       }
       newNode.next = current.next;
       current.next = newNode;
+    }
+  }
+
+  public int deleteAtfirst() {
+
+    if (count == 0) {
+      System.out.println("no element to delete list is empty");
+      return -1;
+    } else if (count == 1) {
+      int val = head.data;
+      head = tail = null;
+      count--;
+      return val;
+    }
+    Node current = head;
+    int x = current.data;
+    current = current.next;
+    head = current;
+    count--;
+    return x;
+  }
+
+  public int deleteAtLast() {
+
+    if (count == 0) {
+      System.out.println("Linked list is empty");
+      return -1;
+    } else if (count == 1) {
+      int x = head.data;
+      head = tail = null;
+      count--;
+      return x;
+    } else {
+      Node prev = head;
+      for (int i = 0; i < count - 2; i++) {
+        prev = prev.next;
+      }
+      int x = tail.data;
+      prev.next = null;
+      tail = prev;
+      count--;
+      return x;
     }
   }
 
@@ -85,6 +131,15 @@ public class Linkedlist {
     ll.addAtPos(1234, 0);
     ll.addAtPos(999, 5);
     ll.addAtPos(30, 3);
+    ll.display();
+    System.out.println();
+    System.out.println("The total number of nodes : " + ll.count);
+
+    System.out.println("deleted element : " + ll.deleteAtfirst());
+
+    System.out.println("deleted element : " + ll.deleteAtLast());
+    System.out.println("deleted element : " + ll.deleteAtLast());
+    System.out.print("linked list after deletion : ");
     ll.display();
   }
 
