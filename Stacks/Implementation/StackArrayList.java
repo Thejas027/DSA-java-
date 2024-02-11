f53595bdaeb5b9c9da4caad82f5beaef07887819
Thejas <teju270934@gmail.com>
2024-02-11 23:01:51 +0530

    Stacks operations using ArrayLists

--- Stacks/Implementation/StackArrayList.java
+++ Stacks/Implementation/StackArrayList.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class StackArrayList {
 
-  static class stack {
+  static class Customstack {
     static ArrayList<Integer> list = new ArrayList<>();
 
     public static boolean isEmpty() {
@@ -14,19 +14,25 @@ public class StackArrayList {
     }
 
     public static int pop() {
+      if (isEmpty()) {
+        throw new IllegalStateException("Styack is Empty !");
+      }
       int top = list.get(list.size() - 1);
       list.remove(list.size() - 1);
       return top;
     }
 
     public static int peek() {
+      if (isEmpty()) {
+        throw new IllegalStateException("Stack is empty !");
+      }
       return list.get(list.size() - 1);
     }
   }
 
   @SuppressWarnings(value = "static-access")
   public static void main(String[] args) {
-    stack s = new stack();
+    Customstack stack = new Customstack();
     Scanner sc = new Scanner(System.in);
     int value, choice;
     do {
@@ -35,24 +41,25 @@ public class StackArrayList {
       System.out.println("2.To Pop the element from the stack");
       System.out.println("3.To Peek the top most element from the stack");
       System.out.println("4.To display the stack elements");
+      System.out.println("5.To exit the program");
       System.out.println("Enter your choice");
       choice = sc.nextInt();
       switch (choice) {
         case 1:
           System.out.println("Enter the value to push into stack");
           value = sc.nextInt();
-          s.push(value);
+          stack.push(value);
           break;
 
         case 2:
-          System.out.println("The popped element from stack : " + s.pop());
+          System.out.println("The popped element from stack : " + stack.pop());
           break;
 
         case 3:
-          System.out.println("The top most element in stack : " + s.peek());
+          System.out.println("The top most element in stack : " + stack.peek());
           break;
         case 4:
-          System.out.println("Stack elements : " + s.list);
+          System.out.println("Stack elements : " + stack.list);
           break;
         case 5:
           System.out.println("Exiting the program..");
