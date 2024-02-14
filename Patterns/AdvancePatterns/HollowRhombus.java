50fc32d9bf408e0c16e24a4739cba15a7de6d897
Thejas <teju270934@gmail.com>
2024-02-15 00:02:47 +0530

    pattern folder adjusted

---
+++ Patterns/AdvancePatterns/HollowRhombus.java
@@ -0,0 +1,22 @@
+package Patterns.AdvancePatterns;
+
+public class HollowRhombus {
+  public static void main(String[] agrs) {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+
+      for (int j = 1; j <= 5; j++) {
+        if (i == 1 || j == 1 || i == n || j == n) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+}
