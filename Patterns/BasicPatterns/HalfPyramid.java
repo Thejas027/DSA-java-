8baee0abfad3afc6887534e0a850cf63baec5d59
Thejas <teju270934@gmail.com>
2024-02-24 16:29:34 +0530

    half pyramid pattern with * and numbers menu driven program

--- Patterns/BasicPatterns/HalfPyramid.java
+++ Patterns/BasicPatterns/HalfPyramid.java
@@ -4,7 +4,9 @@ package Patterns.BasicPatterns;
  * MENU DRIVEN PROGARAM OF SOME "VARIOUS-PYRAMIDS PATTERNS"
  * 1.HALF PYRAMID
  * 2.INVERTED HALF PYRAMID
- * 3. 
+ * 3.HALF PYRAMID WITH 180 deg ROTATION
+ * 4.HALF PYRAMID WITH NUMBERS
+ * 5.INVERTED HALF PYRAMID WITH NUMBERS
  */
 
 import java.util.Scanner;
@@ -36,7 +38,7 @@ public class HalfPyramid {
     }
   }
 
-  // inverted half pyramid with rotation of 180 deg
+  // fnx for half pyramid with rotation of 180 deg
 
   public static void rotatedPyramid() {
     System.out.print("enter the n value : ");
@@ -50,17 +52,42 @@ public class HalfPyramid {
       }
       System.out.println();
     }
+  }
 
+  // fnx for half pyramid with numbers
+
+  public static void numPyramid() {
+    System.out.print("enter the n value : ");
+    int n = sc.nextInt();
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++)
+        System.out.print(j + " ");
+      System.out.println();
+    }
+  }
+
+  // fnx for inverted half pyramid with numbers
+
+  public static void invertedNumPy() {
+    System.out.print("enter the n value : ");
+    int n = sc.nextInt();
+    for (int i = n; i >= 1; i--) {
+      for (int j = 1; j <= i; j++)
+        System.out.print(j + " ");
+      System.out.println();
+    }
   }
 
   public static void main(String[] agrs) {
-    Scanner sc = new Scanner(System.in);
+
     int choice;
     do {
       System.err.println("\n----MENU----\n");
       System.out.println("1.Half Pyramid");
       System.out.println("2.Inverted half Pyramid");
-      System.out.println("3.Inverted half pyramid with 180 deg rotation");
+      System.out.println("3.Half pyramid with 180 deg rotation");
+      System.out.println("4.Half pyramid with numbers");
+      System.out.println("5.Inverted half pyramid with numbers");
       System.out.println("Enter your choice");
       choice = sc.nextInt();
 
@@ -74,6 +101,13 @@ public class HalfPyramid {
           break;
         case 3:
           rotatedPyramid();
+          break;
+
+        case 4:
+          numPyramid();
+          break;
+        case 5:
+          invertedNumPy();
           break;
         default:
           System.out.println("Invalid choice");
